adb2f0354f1117393020f501f7fdee69008d404b
Unknown <flaw561@gmail.com>
2019-04-10 17:30:03 +0600

    third task

---
+++ filetree/cmake-build-debug/CMakeFiles/filetree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filetree.dir/main.cpp.o"
+  "filetree.pdb"
+  "filetree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filetree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
